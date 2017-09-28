0e23a7c86543f1265ff222fda5d4e8b46578c53c
Achim Vandierendonck <Achim.Vandierendonck@UGent.be>
2017-09-28 18:05:12 +0200

    Create event overview with templates

--- views/events.php
+++ views/events.php
@@ -18,7 +18,7 @@ try {
     if (!$statement)
         throw new Exception("Database error.");
     $statement->execute(array(':orderColumn' => 'startTime', ':currentTime' => date('Y-m-d H:i:s')));
-    var_dump($statement->fetchAll(PDO::FETCH_CLASS, 'Event'));
+    $events = $statement->fetchAll(PDO::FETCH_CLASS, 'Event');
 
     // Close DB connection
     $db = null;
@@ -26,4 +26,104 @@ try {
     exit(header("Location: /500/"));
 }
 ?>
+
+<div id="events">
+    <div class="columns">
+        <div class="column is-2 is-narrow">
+            <aside class="menu">
+                <p class="menu-label">Academiejaar</p>
+                <ul class="menu-list">
+                    <li>
+                        <a href="" class="is-active">'17 - '18</a>
+                    </li>
+                </ul>
+            </aside>
+        </div>
+        <div class="column">
+            <div class="content"><h1>Toekomstige evenementen</h1></div>
+            <div class="tile is-ancestor">
+                <div class="tile is-parent">
+                    <div class="tile is-child box">
+                        <article class="content">
+                            <h1 class="title">Title</h1>
+                            <p class="subtitle">Subtitle</p>
+                            <p>Teaser</p>
+                        </article>
+                    </div>
+                </div>
+                <div class="tile is-parent">
+                    <div class="tile is-child box ceneka-red">
+                        <article class="content">
+                            <h1 class="title">Title</h1>
+                            <p class="subtitle">Subtitle</p>
+                            <p>Teaser</p>
+                        </article>
+                    </div>
+                </div>
+            </div>
+            <div class="tile is-ancestor">
+                <div class="tile is-parent">
+                    <div class="tile is-child box ceneka-texture">
+                        <article class="content">
+                            <h1 class="title">Title</h1>
+                            <p class="subtitle">Subtitle</p>
+                            <p>Teaser</p>
+                        </article>
+                    </div>
+                </div>
+                <div class="tile is-parent">
+                    <div class="tile is-child box ceneka-grey">
+                        <article class="content">
+                            <h1 class="title">Title</h1>
+                            <p class="subtitle">Subtitle</p>
+                            <p>Teaser</p>
+                        </article>
+                    </div>
+                </div>
+            </div>
+            <div class="content"><h1>Verleden evenementen</h1></div>
+            <div class="tile is-ancestor">
+                <div class="tile is-parent">
+                    <div class="tile is-child box">
+                        <article class="content">
+                            <h1 class="title">Title</h1>
+                            <p class="subtitle">Subtitle</p>
+                            <p>Teaser</p>
+                        </article>
+                    </div>
+                </div>
+                <div class="tile is-parent">
+                    <div class="tile is-child box ceneka-red">
+                        <article class="content">
+                            <h1 class="title">Title</h1>
+                            <p class="subtitle">Subtitle</p>
+                            <p>Teaser</p>
+                        </article>
+                    </div>
+                </div>
+            </div>
+            <div class="tile is-ancestor">
+                <div class="tile is-parent">
+                    <div class="tile is-child box ceneka-texture">
+                        <article class="content">
+                            <h1 class="title">Title</h1>
+                            <p class="subtitle">Subtitle</p>
+                            <p>Teaser</p>
+                        </article>
+                    </div>
+                </div>
+                <div class="tile is-parent">
+                    <div class="tile is-child box ceneka-grey">
+                        <article class="content">
+                            <h1 class="title">Title</h1>
+                            <p class="subtitle">Subtitle</p>
+                            <p>Teaser</p>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <?php include_once 'layouts/main/footer.php' ?>
