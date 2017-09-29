e388ba709fc88597c6691a8fe1a7052ad1d841f6
tverum <vanerum.tim@icloud.com>
2017-09-29 23:12:43 +0200

    Vernieuwde lay-out

---
+++ views/aux.php
@@ -0,0 +1,195 @@
+<div class="column is-3 is-centered is-vertical">
+            <div class="tile is-parent is-12">
+                <div class="tile card is-child is-vertical">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                            <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-content">
+                                <p class="title is-5">Achim Vandierendonck</p>
+                                <p class="subtitle is-7 is-link"><a href="mailto:praeses@ceneka.ugent.be">praeses@ceneka.be</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="tile is-parent is-12">
+                <div class="tile card is-child is-vertical">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                            <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-content">
+                                <p class="title is-5">Jeroen Flipts</p>
+                                <p class="subtitle is-7 is-link"><a href="mailto:career@ceneka.ugent.be">career@ceneka.be</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="tile is-parent is-12">
+                <div class="tile card is-child is-vertical">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                            <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-content">
+                                <p class="title is-5">Nick Barbé</p>
+                                <p class="subtitle is-7 is-link"><a href="mailto:feest@ceneka.be">feest@ceneka.be</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="column is-3 is-centered is-vertical">
+            <div class="tile is-parent is-12">
+                <div class="tile card is-child is-vertical">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                            <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-content">
+                                <p class="title is-5">Louise Catthoor</p>
+                                <p class="subtitle is-7 is-link"><a href="mailto:praeses@ceneka.ugent.be">praeses@ceneka.be</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="tile is-parent is-12">
+                <div class="tile card is-child is-vertical">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                            <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-content">
+                                <p class="title is-5">Dennis Maes</p>
+                                <p class="subtitle is-7 is-link"><a href="mailto:career@ceneka.ugent.be">career@ceneka.be</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="tile is-parent is-12">
+                <div class="tile card is-child is-vertical">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                            <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-content">
+                                <p class="title is-5">Tim Vandamme</p>
+                                <p class="subtitle is-7 is-link"><a href="mailto:web@ceneka.be">web@ceneka.be</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="column is-3 is-centered is-vertical">
+            <div class="tile is-parent is-12">
+                <div class="tile card is-child is-vertical">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                            <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-content">
+                                <p class="title is-5">Bjorn Claes</p>
+                                <p class="subtitle is-7 is-link"><a href="mailto:praeses@ceneka.ugent.be">penning@ceneka.be</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="tile is-parent is-12">
+                <div class="tile card is-child is-vertical">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                            <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-content">
+                                <p class="title is-5">Aren Maes</p>
+                                <p class="subtitle is-7 is-link"><a href="mailto:career@ceneka.ugent.be">career@ceneka.be</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="tile is-parent is-12">
+                <div class="tile card is-child is-vertical">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                            <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-content">
+                                <p class="title is-5">Tim Van Erum</p>
+                                <p class="subtitle is-7 is-link"><a href="mailto:web@ceneka.be">web@ceneka.be</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="column is-3 is-centered is-vertical">
+            <div class="tile is-parent is-12">
+                <div class="tile card is-child is-vertical">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                            <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-content">
+                                <p class="title is-5">Rémi Delanghe</p>
+                                <p class="subtitle is-7 is-link"><a href="mailto:praeses@ceneka.ugent.be">externe@ceneka.be</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="tile is-parent is-12">
+                <div class="tile card is-child is-vertical">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                            <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-content">
+                                <p class="title is-5">Jarne Verhaeghe</p>
+                                <p class="subtitle is-7 is-link"><a href="mailto:interne@ceneka.be">interne@ceneka.be</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
--- views/bestuur.php
+++ views/bestuur.php
@@ -1,7 +1,7 @@
 <?php include_once 'layouts/main/header.php'?>
-<div id="about" class="columns">
-<div class="column is-8 is-offset-2">
-    <div class="tile is-vertical is-ancestor ">
+<div id="about" class="columns is-centered">
+<div class="column is-12">
+    <div class="tile is-vertical is-ancestor">
         <div class="subnav tabs is-centered">
             <ul>
                 <li>
@@ -20,23 +20,172 @@
         </div>
     </div>
     <h1 class="title is-1 has-text-centered">Bestuur CenEka</h1>
-    <div class="tile is-ancestor">
-        <div class="tile is-parent is-4">
-            <div class="tile card is-child is-vertical">
-		        <div class="card-image">
-			        <figure class="image is-4by3">
-      				    <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-    			    </figure>
-		        </div>
-		        <div class="card-content">
-    			    <div class="media">
-      				    <div class="media-content">
-        				    <p class="title is-4">John Smith</p>
-        				    <p class="subtitle is-6 is-link"><a href="mailto:lol@ceneka.ugent.be">lol@ceneka.ugent.be</a></p>
-      				    </div>
-    			    </div>
-	            </div>
+    <div class="tile is-ancestor is-vertical">
+        <div class="tile is-parent">
+            <div class="tile is-parent is-8">
+                <div class="tile is-child box is-12 is-vertical">
+                    <h1 class="has-text-centered">Praeses | Vice-Praeses</h1>
+                    <div class="tile is-parent is-12">
+                        <div class="tile is-parent is-6">
+                            <div class="tile card is-child is-vertical">
+                                <div class="card-image">
+                                    <figure class="image is-4by3">
+                                        <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                                    </figure>
+                                </div>
+                                <div class="card-content">
+                                    <div class="media">
+                                        <div class="media-content">
+                                            <p class="title is-5">Achim Vandierendonck</p>
+                                            <p class="subtitle is-7 is-link"><a>@denGrotenBaas</a></p>
+                                        </div>
+                                    </div>
+                                    <div class="content">
+                                        Als praeses is Achim het voornaamste aanspreekpunt van de vereniging. Hij vertegenwoordigd CenEka op
+                                        allerlei events doorheen het academiejaar. Dit houdt hem anderzijds allerminst tegen om actief mee te werken
+                                        aan onze activiteiten.<br>
+                                        Mail: <a href="mailto:praeses@ceneka.be">praeses@ceneka.be</a>
+                                    </div> 
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tile is-parent is-6">
+                            <div class="tile card is-child is-vertical">
+                                <div class="card-image">
+                                    <figure class="image is-4by3">
+                                        <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                                    </figure>
+                                </div>
+                                <div class="card-content">
+                                    <div class="media">
+                                        <div class="media-content">
+                                            <p class="title is-5">Louise Catthoor</p>
+                                            <p class="subtitle is-7 is-link"><a>@denKleinenBaas</a></p>
+                                        </div>
+                                    </div>
+                                    <div class="content">
+                                        Ons kleinste lid, de vice-praeses, is de persoon bij uitstek die het reilen en zeilen binnen de vereniging 
+                                        bijhoudt en coördineerd. Door een goed team te vormen met de praeses leiden zij alle activiteiten in goede
+                                        banen <br>
+                                        Mail: <a href="mailto:praeses@ceneka.be">praeses@ceneka.be</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
+            
+            <div class="tile is-parent is-4 is-vertical">
+                <div class="tile is-child is-12">
+                    <article class="content box column">
+                        <h1 class="has-text-centered">Medewerker worden?</h1>
+                        <div class="description">
+                            Help je graag mee bij het organiseren van allerhande diverse activiteiten, gaande van
+                            jobfairs, over lezingen tot natuurlijk de (in ons geval iets minder) traditionele
+                            studentenfeestjes? Laat dan zeker iets weten aan onze geheeleigen Interne verantwoordelijke,
+                            <a href="mailto:interne@ceneka.ugent.be">Jarne Verhaeghe</a>. Hij zal jou hier zeker meer
+                            informatie over kunnen verschaffen.
+                        </div>
+                    </article>
+                </div>
+                <div class="tile is-child is-12">
+                <article class="content box column">
+                        <h1 class="has-text-centered">Bestuurslid worden?</h1>
+                        <div class="description">
+                            Geïnteresseerd in een aanzienlijkere rol dan gewoon medewerker? Ben je bereid (en klaar) 
+                            om een grotere verantwoordelijkheid op te nemen? Of heb je gewoon een fenomenale visie 
+                            en bijbehorende toewijding om een studentenvereniging te runnen, dan ben jij misschien
+                            wel geschikt om tot ons bestuur toe te treden. Tegen het einde van het jaar organiseert 
+                            CenEka steeds verkiezingen. Laat zeker dit jaar je kans niet schieten om een uniek jaar
+                            te beleven.
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </div>
+        
+
+        <div class="tile is-parent is-12">
+            <div class="tile is-child box is-12 is-vertical">
+                <h1 class="has-text-centered">Career Team</h1>
+                <div class="tile is-parent is-12">
+                    <div class="tile is-parent is-4">
+                        <div class="tile card is-child is-vertical">
+                            <div class="card-image">
+                                <figure class="image is-4by3">
+                                    <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                                </figure>
+                            </div>
+                            <div class="card-content">
+                                <div class="media">
+                                    <div class="media-content">
+                                        <p class="title is-5">Jeroen Flipts</p>
+                                        <p class="subtitle is-7 is-link"><a>@theNegotiator</a></p>
+                                    </div>
+                                </div>
+                                <div class="content">
+                                    Als meest ervaren lid van het career team brengt Jeroen veel kennis van zaken met zich mee in
+                                    verband met het organiseren van de EMECS Career Day (Fair?). Hij zorgt voor een nuchtere kijk
+                                    en een kritische geest<br>
+                                    Mail: <a href="mailto:career@ceneka.be">career@ceneka.be</a>
+                                </div> 
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tile is-parent is-4">
+                        <div class="tile card is-child is-vertical">
+                            <div class="card-image">
+                                <figure class="image is-4by3">
+                                    <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                                </figure>
+                            </div>
+                            <div class="card-content">
+                                <div class="media">
+                                    <div class="media-content">
+                                        <p class="title is-5">Dennis Maes</p>
+                                        <p class="subtitle is-7 is-link"><a>@theFilosopher</a></p>
+                                    </div>
+                                </div>
+                                <div class="content">
+                                    Een frisse wind en een professionele aanpak. Zo kunnen we het best de invloed van Dennis in het
+                                    Career Team beschrijven. Met veel goede ideëen en een vernieuwd perspectief zorgt hij voor een 
+                                    <i>positive vibe</i><br>
+                                    Mail: <a href="mailto:career@ceneka.be">career@ceneka.be</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tile is-parent is-4">
+                        <div class="tile card is-child is-vertical">
+                            <div class="card-image">
+                                <figure class="image is-4by3">
+                                    <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                                </figure>
+                            </div>
+                            <div class="card-content">
+                                <div class="media">
+                                    <div class="media-content">
+                                        <p class="title is-5">Aren Maes</p>
+                                        <p class="subtitle is-7 is-link"><a>@theSilentKiller</a></p>
+                                    </div>
+                                </div>
+                                <div class="content">
+                                    Op een rustige en bescheiden manier is Aren de stille werkkracht binnen het Career Team. 
+                                    Hij doet zijn werk altijd op zijn eigen tempo, maar je kan er steeds zeker van zijn dat 
+                                    het af zal geraken<br>
+                                    Mail: <a href="mailto:career@ceneka.be">career@ceneka.be</a>
+                                </div> 
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 </div>
