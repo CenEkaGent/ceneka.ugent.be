dc3800b523967a49b5c3fc518d1ab970c7d172da
Maarten Moens <Maarten.Moens@UGent.be>
2018-02-08 17:36:16 +0100

    Testfix 3

--- cas-data.php
+++ cas-data.php
@@ -4,4 +4,5 @@ include_once($CASPATH);
 phpCAS::client(SAML_VERSION_1_1,'login.ugent.be',443,'', true, 'saml');
 phpCAS::handleLogoutRequests(true, array('cas1.ugent.be','cas2.ugent.be','cas3.ugent.be','cas4.ugent.be','cas5.ugent.be','cas6.ugent.be'));
 phpCAS::setCasServerCACert('/etc/ssl/certs/ca-certificates.crt');
+phpCAS::setFixedServiceURL 	('http://ceneka.ugent.be/')
 ?>
