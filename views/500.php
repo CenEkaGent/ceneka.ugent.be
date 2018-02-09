f0a751b35762d853d1a9c480f7481730ae70cdd5
Maarten Moens <Maarten.Moens@UGent.be>
2018-02-09 15:33:52 +0100

    Debug

--- views/500.php
+++ views/500.php
@@ -2,12 +2,14 @@
 <div id="500">
     <div class="tile is-vertical is_parent">
         <pre class="tile is-child content is-hidden-mobile" style="background: unset;">
+            <!--
             #include &lt;iostream&gt;;<br>
             <br>
             int main(int argc, char **argv) {<br>
                 std::cout &lt;&lt; &quot;Error 500, you seems to have gotten to the wrong place&quot;;<br>
                 return 0;<br>
-            }<br>
+            }<br>-->
+            Hey
         </pre>
         <pre class="tile is-child content is-hidden-mobile">
         
