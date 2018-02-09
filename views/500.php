c993bebe3c1d611f448e1d729ce0b25e408051e3
Maarten Moens <Maarten.Moens@UGent.be>
2018-02-09 15:34:45 +0100

    Debug

--- views/500.php
+++ views/500.php
@@ -1,16 +1,16 @@
 <?php include_once 'layouts/main/header.php' ?>
 <div id="500">
     <div class="tile is-vertical is_parent">
-        <pre class="tile is-child content is-hidden-mobile" style="background: unset;">
-            <!--
+        <!--<pre class="tile is-child content is-hidden-mobile" style="background: unset;">
+            
             #include &lt;iostream&gt;;<br>
             <br>
             int main(int argc, char **argv) {<br>
                 std::cout &lt;&lt; &quot;Error 500, you seems to have gotten to the wrong place&quot;;<br>
                 return 0;<br>
-            }<br>-->
+            }<br>
             Hey
-        </pre>
+        </pre>-->He
         <pre class="tile is-child content is-hidden-mobile">
         
             Last login: Sun Sep 10 21:03:08 on ttys001
