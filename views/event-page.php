8b7fd5e2e5c62b064b95284c719c2969cdac3d4d
Maarten Moens <Maarten.Moens@UGent.be>
2018-02-09 12:05:09 +0100

    Debug

--- views/event-page.php
+++ views/event-page.php
@@ -39,6 +39,7 @@ if (!$statement)
     throw new Exception("Database error.");
 $statement->execute(array(':eventID'=>$data[0]->id));
 $registerable = $statement->fetchAll(PDO::FETCH_CLASS, 'Event');
+print_r($registerable);
 if (sizeof($registerable) == 1 && $registerable[0]['canSubscribe'] == 1){
     $sql = 'SELECT IF (registraties.event_id IS NULL, FALSE, TRUE) AS aanwezig FROM registraties 
     WHERE registraties.event_id = :eventID AND registraties.leden_id = :ledenID';
